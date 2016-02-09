10167b48db5f8e13dae0520f981071fa158ad1c4
Edgar Chaput <chaput.edgar@gmail.com>
2016-02-09 14:16:52 +0100

    CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,6 +5,33 @@ class TasksController < ApplicationController
   @tasks = Task.all
   end
 
+  def show
+  end
+
+  def new
+  @task = Task.new
+  end
+
+  def edit
+  end
+
+  def create
+  @task = Task.new(task_params)
+  @task.save
+  redirect_to task_path(@task)
+  end
+
+  def update
+  @task.update(task_params)
+  redirect_to task_path(@task)
+  end
+
+  def destroy
+  @task.delete
+  redirect_to tasks_path
+  end
+
+
   private
 
   def task_params
@@ -12,7 +39,7 @@ class TasksController < ApplicationController
   end
 
   def set_task
-  @task  = Task.find(params[:id])
+  @task = Task.find(params[:id])
   end
 
 end
